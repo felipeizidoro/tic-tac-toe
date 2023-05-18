c4454e6335f2b0cd9d5cbacd4cecb3b6b74c59d1
Felipe Izidoro <flp.izidoro@hotmail.com>
2023-05-18 07:06:16 +0100

    Update Javascript
    Verification win region

--- src/js/script.js
+++ src/js/script.js
@@ -20,7 +20,48 @@ function initializeGame(){
 };
 
 function handleBoardClick(ev){
+    const span = ev.currentTarget;
+    const region = span.dataset.region;
+    const rowColumnPair = region.split('.');
+    const row = rowColumnPair[0];
+    const column = rowColumnPair[1];
+    if(turnPlayer === 'playerOne'){
+        span.innerText = 'X';
+        virtualBoard[row][column] = 'X';
+    } else {
+        span.innerText = 'O';
+        virtualBoard[row][column] = 'O';
+    };
+    console.clear();
+    console.table(virtualBoard);
+    disableClick(span);
+    const winRegion = getWinRegion();
+};
 
+function disableClick(element){
+    element.style.cursor = 'default';
+    element.removeEventListener('click', handleBoardClick);
 };
 
 document.getElementById('start').addEventListener('click', initializeGame);
+
+function getWinRegion(){
+    const winRegion = [];
+    if(virtualBoard[0][0] && virtualBoard[0][0] === virtualBoard[0][1] && virtualBoard[0][0] === virtualBoard[0][2]);
+    winRegion.push('0.0','0.1','0.2');
+    if(virtualBoard[1][0] && virtualBoard[1][0] === virtualBoard[1][1] && virtualBoard[1][0] === virtualBoard[1][2]);
+    winRegion.push('1.0','1.1','1.2');
+    if(virtualBoard[2][0] && virtualBoard[2][0] === virtualBoard[2][1] && virtualBoard[2][0] === virtualBoard[2][2]);
+    winRegion.push('2.0','2.1','2.2');
+    if(virtualBoard[0][0] && virtualBoard[0][0] === virtualBoard[1][0] && virtualBoard[0][0] === virtualBoard[2][0]);
+    winRegion.push('0.0','1.0','2.0');
+    if(virtualBoard[0][1] && virtualBoard[0][1] === virtualBoard[1][1] && virtualBoard[0][1] === virtualBoard[2][1]);
+    winRegion.push('0.1','1.1','2.1');
+    if(virtualBoard[0][2] && virtualBoard[0][2] === virtualBoard[1][2] && virtualBoard[0][2] === virtualBoard[2][2]);
+    winRegion.push('0.2','1.2','2.2');
+    if(virtualBoard[0][0] && virtualBoard[0][0] === virtualBoard[1][1] && virtualBoard[0][0] === virtualBoard[2][2]);
+    winRegion.push('0.0','1.1','2.2');
+    if(virtualBoard[0][2] && virtualBoard[0][2] === virtualBoard[1][1] && virtualBoard[0][2] === virtualBoard[2][0]);
+    winRegion.push('0.2','1.1','2.0');
+    return winRegion;
+};
